3a1e5cf89e6d65dbb7c6e6eb4d479f8fed753eb8
Jackson Brough <jacksontbrough@gmail.com>
2025-09-02 11:18:17 -0600

    Remove old cubical comments in flake.nix

--- flake.nix
+++ flake.nix
@@ -4,8 +4,6 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
-    # cubical.url = "github:agda/cubical";
-    # cubical.inputs.nixpkgs.follows = "nixpkgs";
   };
 
   outputs = { self, nixpkgs, flake-utils }:
@@ -13,21 +11,12 @@
       (system:
         let
           pkgs = nixpkgs.legacyPackages.${system};
-          # cubical = cubical.packages.${system}.cubical;
-          # gdacubical = cubical-packages.cubical;
-          # agda = pkgs.agda.withPackages [ cubical ];
           agda = pkgs.agda.withPackages [ pkgs.agdaPackages.cubical ];
         in
           {
-            # packages.${system} = {
-            #   cubical = cublical;
-            #   agda = agda;
-            # };
-            # defaultPackages.${system} = agda;
             devShells.default = pkgs.mkShell {
               buildInputs = [
                 agda
-                # agda.withPackages [ cubical ]
               ];
             };
           }
